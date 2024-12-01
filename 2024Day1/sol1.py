f324501cb10a1b6f830fc56b5b7765a255d29bd8
Matthew Ivancic <matthew.ivancic91@gmail.com>
2024-12-01 09:09:08 -0600

    dataset and first solution

---
+++ 2024Day1/sol1.py
@@ -0,0 +1,30 @@
+def get_data():
+    with open('input.txt','r') as f:
+        return f.read()
+
+def process_data(dataset):
+    dataset=dataset.split("\n")
+    processed_dataset=[]
+    for line in dataset:
+        processed_dataset.append(line.split("   "))
+    leftSide =[]
+    rightSide=[]
+    for line in processed_dataset:
+        if len(line)==2:
+            leftSide.append(line[0])
+            rightSide.append(line[1])
+    leftSide.sort()
+    rightSide.sort()
+    return leftSide,rightSide
+
+def compare_datasets(leftSide,rightSide):
+    answer=0
+    for i in range(0,len(leftSide)):
+        answer+=abs(int(leftSide[i])-int(rightSide[i]))
+    return answer
+    
+
+dataset=get_data()
+leftSide,rightSide=process_data(dataset)
+answer=compare_datasets(leftSide,rightSide)
+print(answer)
